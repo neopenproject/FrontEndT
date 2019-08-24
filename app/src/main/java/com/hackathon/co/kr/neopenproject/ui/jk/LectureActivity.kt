8d92a6af04be27a29745c3e4da7728cd98597ed9
jklee103 <jklee103@gmail.com>
2019-08-24 19:52:09 +0900

    ui demo

---
+++ app/src/main/java/com/hackathon/co/kr/neopenproject/ui/jk/LectureActivity.kt
@@ -0,0 +1,17 @@
+package com.hackathon.co.kr.neopenproject.ui.jk
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.hackathon.co.kr.neopenproject.R
+
+import kotlinx.android.synthetic.main.activity_lecture.*
+
+class LectureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
